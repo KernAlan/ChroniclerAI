1243e6d0d8d225d7d6595ca55c0ffeae3b14697b
Alan Kern <akern@bigtime.net>
2023-03-17 00:35:41 -0500

    Finished GUI. Begin wiring up transcribe.

--- BT.Transcription/MainWindow.xaml.cs
+++ BT.Transcription/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,40 +22,81 @@ namespace BT.Transcription
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string? ApiKey { get; set; }
+        public string? AudioFilePathTextBox { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+        }
 
-            // Replace with your OpenAI API key
-            string apiKey = "YOUR_OPENAI_API_KEY";
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
-            // Replace with the model name and file path
-            string modelName = "whisper-1";
-            string filePath = "C:\\path\\to\\file\\openai.mp3";
+        }
 
-            // Create a new HttpClient
-            HttpClient client = new HttpClient();
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
 
-            // Set the Authorization header
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+        }
 
-            // Create a new multipart/form-data content
-            MultipartFormDataContent content = new MultipartFormDataContent();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
 
-            // Add the model name field
-            content.Add(new StringContent(modelName), "model");
+        private async Task<string> Transcribe()
+        {
+            try
+            {
+                if (AudioFilePathTextBox is null)
+                {
+                    throw new ArgumentNullException("No audio file detected!");
+                }
 
-            // Add the file field
-            byte[] fileBytes = File.ReadAllBytes(filePath);
-            ByteArrayContent fileContent = new ByteArrayContent(fileBytes);
-            fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("audio/mpeg");
-            content.Add(fileContent, "file", Path.GetFileName(filePath));
+                if (ApiKey is null)
+                {
+                    throw new ArgumentNullException("No API key detected!");
+                }
 
-            // Send the API request
-            HttpResponseMessage response = client.PostAsync("https://api.openai.com/v1/audio/transcriptions", content).Result;
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "TOKEN");
 
-            // Print the API response
-            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                    using (var content = new MultipartFormDataContent())
+                    {
+                        content.Add(new StreamContent(File.OpenRead(AudioFilePathTextBox)), "file", System.IO.Path.GetFileName(AudioFilePathTextBox));
+                        content.Add(new StringContent("whisper-1"), "model");
+
+                        using (var response = await client.PostAsync("https://api.openai.com/v1/audio/transcriptions", content))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var responseContent = await response.Content.ReadAsStringAsync();
+                                return responseContent;
+                            }
+                            else
+                            {
+                                throw new Exception($"Request failed with status code {response.StatusCode}: {await response.Content.ReadAsStringAsync()}");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+           
+        }
+
+
+        private void Browse(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                AudioFilePathTextBox = openFileDialog.FileName;
+            }
         }
     }
 }
