fb05667dff893bd791fc3db2020cb05a45135ca4
Alan Kern <akern@bigtime.net>
2023-03-20 23:49:35 -0500

    Release and debug clean and rebuild.

--- ChroniclerAI/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ ChroniclerAI/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace ChroniclerAI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ChroniclerAI;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ChroniclerAI;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
